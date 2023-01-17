4b4496596b769efbc2e7ba70d8920d8fa8781921
sharlisbg <sharislizza15@gmail.com>
2023-01-16 22:10:44 -0600

    Clase multiplicacion

---
+++ Multiplicacion.java
@@ -0,0 +1,24 @@
+/*
+ * @author Sharis Barrios
+ */
+
+ public class Multiplicacion{
+
+    /*
+     * Constructor de Sumas
+     */
+    public Multiplicacion() {
+    }
+
+    
+    /** 
+     * @param num1
+     * @param num2
+     * @return int
+     */
+    public int Multiplicar(int num1, int num2){
+        int mult = num1*num2;
+        return mult;
+    }
+ }
+
