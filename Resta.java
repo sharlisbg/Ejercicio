d0eb8e57a68a1d492246c73f9a94a60d68b855a8
sharlisbg <sharislizza15@gmail.com>
2023-01-16 22:11:12 -0600

    Revert "Revert "Clase Resta""
    This reverts commit e869be7bdbe2bf7ca9c6a7ddc68dc736e9eda861.

---
+++ Resta.java
@@ -0,0 +1,24 @@
+/*
+ * @author Sharis Barrios
+ */
+
+ public class Resta{
+
+    /*
+     * Constructor de Sumas
+     */
+    public Resta() {
+    }
+
+    
+    /** 
+     * @param num1
+     * @param num2
+     * @return int
+     */
+    public int Restar (int num1, int num2){
+        int resta = num1 - num2;
+        return resta;
+    }
+
+ }
