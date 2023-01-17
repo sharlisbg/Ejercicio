60c19f9a613b450b3fa2d35236178f970a73cdc5
sharlisbg <sharislizza15@gmail.com>
2023-01-16 22:07:57 -0600

    Clase Suma

---
+++ Suma.java
@@ -0,0 +1,23 @@
+/*
+ * @author Sharis Barrios
+ */
+
+public class Suma{
+
+    /*
+     * Constructor de Sumas
+     */
+    public Suma() {
+    }
+
+    
+    /** 
+     * @param num1
+     * @param num2
+     * @return int
+     */
+    public int Sumar(int num1, int num2){
+        int suma = num1 + num2;
+        return suma;
+    }
+}
